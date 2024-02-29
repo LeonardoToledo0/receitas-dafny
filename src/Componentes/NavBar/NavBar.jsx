08a451629a8b12cf8dd7c6a7770375a9d3837717
Leonardo Toledo <leotoledo010@gmail.com>
2024-02-29 15:36:22 -0300

    Estrutura Receitas Dafny

---
+++ src/Componentes/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from './NavBarStyles';
+
+export const NavBar = () => {
+    return (
+        <S.NavHeader>
+            <S.NavTitle>leo</S.NavTitle>
+
+        </S.NavHeader>
+    );
+};
---
+++ src/Componentes/NavBar/NavBarStyles.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+
+export const NavHeader = styled.header``
+export const NavTitle = styled.h1``
